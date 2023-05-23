d18c68890714eb8b1f015ad1092999acce8e8531
kimspractice <tpghksdldia@naver.com>
2023-05-23 14:43:29 +0900

    SEO 만듬

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,21 +8,45 @@ export default function NavBar() {
 
   return (
     <>
-      <Link href={"/"} className={`${path === "/" ? "active" : ""}`}>
-        HOME
-      </Link>
-      <Link href={"/about"} className={`${path === "/about" ? "active" : ""}`}>
-        ABOUT
-      </Link>
+      <nav>
+        <div>
+          <Link href={"/"} className={`${path === "/" ? "active" : ""}`}>
+            HOME
+          </Link>
+          <Link
+            href={"/about"}
+            className={`${path === "/about" ? "active" : ""}`}
+          >
+            ABOUT
+          </Link>
+        </div>
+      </nav>
 
       <style jsx global>{`
-        a {
-          font-size: 32px;
-          text-decoration: none;
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
         .active {
           color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </>
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "about",
+  title: "About",
 };
 
 export default function about() {
